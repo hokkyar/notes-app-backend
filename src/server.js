b2986e84ddcf19a128b9e334066a5370c558aa50
Hokky Aryasta <hokkyaryasta@gmail.com>
2023-01-24 12:09:37 +0800

    auto binding and error handling fixed

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const notes = require('./api/notes')
 const NotesService = require('./services/postgres/NotesService')
 const NotesValidator = require('./validator/notes')
 
+const ClientError = require('./exceptions/ClientError')
+
 const init = async () => {
   const notesService = new NotesService()
 
@@ -16,6 +18,35 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  })
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      })
+      newResponse.code(500)
+      return newResponse
+    }
+    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+    return h.continue
   })
 
   await server.register({
